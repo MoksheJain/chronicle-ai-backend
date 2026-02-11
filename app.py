83ca166c578ded4b0fbdbb3faa350ac2732c2c2c
turk-911 <jainmokshejain2005@gmail.com>
2026-02-11 16:02:09 +0530

    Fix: added logging

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, EmailStr
 from tools.sheet_manager import add_subscriber
 from tools.email_sender import send_welcome_email
 from fastapi.middleware.cors import CORSMiddleware
+import traceback
 
 app = FastAPI()
 app.add_middleware(
@@ -16,17 +17,17 @@ app.add_middleware(
 class Subscriber(BaseModel):
     email: EmailStr   # automatic email validation
 
+
 @app.post("/subscribe")
 def subscribe(subscriber: Subscriber):
 
     try:
-        # 1. Add to Google Sheet
         add_subscriber(subscriber.email)
-
-        # 2. Send Welcome Email
         send_welcome_email(subscriber.email)
 
-        return {"message": "Subscribed successfully. Check your email!"}
+        return {"message": "Subscribed successfully"}
 
     except Exception as e:
+        print("ERROR OCCURRED:")
+        traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
