f19d215f9cd78949b64e09a54f2134ab272f7427
turk-911 <jainmokshejain2005@gmail.com>
2026-02-11 16:10:00 +0530

    Fix: fixed more issues :/

--- tools/sheet_manager.py
+++ tools/sheet_manager.py
@@ -5,7 +5,15 @@ from google.oauth2.service_account import Credentials
 
 def get_sheet():
 
-    creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+    creds_raw = os.getenv("GOOGLE_CREDENTIALS")
+
+    if not creds_raw:
+        raise Exception("GOOGLE_CREDENTIALS not found in environment variables")
+
+    creds_dict = json.loads(creds_raw)
+
+    # 🔥 Fix newline formatting issue
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
 
     credentials = Credentials.from_service_account_info(
         creds_dict,
@@ -14,7 +22,9 @@ def get_sheet():
 
     client = gspread.authorize(credentials)
 
-    sheet = client.open("ChronicleAI Subscribers").sheet1
+    sheet = client.open_by_key(
+        "16mc8lBDL-6qEVXvkAC62ArKdSkZPlGOd52lq2dbnVLA"
+    ).sheet1
 
     return sheet
 
