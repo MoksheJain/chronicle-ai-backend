3cd061456eaa524bf17496d571f30e530dfb79cf
turk-911 <jainmokshejain2005@gmail.com>
2026-02-11 13:31:19 +0530

    Fix: cors issues

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
 from tools.sheet_manager import add_subscriber
 from tools.email_sender import send_welcome_email
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Subscriber(BaseModel):
     email: EmailStr   # automatic email validation
